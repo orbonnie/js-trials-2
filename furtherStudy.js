02158cec5fa15f2b585503028b96c4312fa9c1a6
Bonnie Owens <arlolocos@gmail.com>
2022-11-19 18:49:00 -0500

    Completed further study

--- furtherStudy.js
+++ furtherStudy.js
@@ -1,9 +1,30 @@
 'use strict';
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  const words2Set = new Set(words2);
+
+  return words1.filter(word => words2Set.has(word));
 }
 
 function kidsGame(names) {
-  // Replace this with your code
+  const first = names.shift();
+  const answer = [first];
+
+  while (names.length) {
+    let ansLen = answer.length;
+
+    for (let i = 0; i < names.length; i++) {
+      let last = answer[answer.length - 1];
+      if (names[i][0] === last[last.length - 1]) {
+        answer.push(names[i]);
+        names.splice(i, 1);
+        break;
+      }
+    }
+    if (ansLen === answer.length) {
+      break;
+    }
+  }
+
+  return answer;
 }
