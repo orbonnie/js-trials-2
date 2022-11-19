b7578050b6f7b3cc2979e7c08d7fc7cf1d501da6
Bonnie Owens <arlolocos@gmail.com>
2022-11-19 18:15:34 -0500

    Completed objects

--- objects.js
+++ objects.js
@@ -1,11 +1,29 @@
-'use strict';
+"use strict";
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  const words = {};
+
+  phrase.split(' ').forEach((word) => {
+    if (words[word]) {
+      words[word] += 1;
+    } else {
+      words[word] = 1;
+    }
+  });
+
+  return words;
 }
+
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  const melon_prices = {
+    2.5: ["Cantaloupe", "Honeydew"],
+    2.95: ["Watermelon"],
+    3.25: ["Musk", "Crenshaw"],
+    14.25: ["Christmas"],
+  };
+
+  return melon_prices[price];
 }
